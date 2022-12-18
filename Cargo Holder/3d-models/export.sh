4a15e5cc43300eb256681fe5cf6eae6a6620a8d2
Andreas Menzel <mail@andreas-menzel.com>
2022-12-18 16:08:44 +0100

    Update stl files export script

--- Cargo Holder/3d-models/export.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script is used to create the stl-files for the cargo holder mechanism.
-# Make sure you have OpenSCAD installed and can use it via the terminal.
-
-openscad "./openscad files/container_part.scad" -o "./stl files/container_part.stl" -D "export=true;"
-openscad "./openscad files/drone_part.scad" -o "./stl files/drone_part.stl" -D "export=true;"
-
-openscad "./openscad files/assembled.scad" -o "./stl files/assembled.stl" -D "export=true;"
---
+++ Cargo Holder/scripts/export_stl_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script is used to create the stl-files for the cargo holder mechanism.
+# Make sure you have OpenSCAD installed and can use it to export models through
+# the terminal.
+
+mkdir -p "../3d-models/stl files"
+
+openscad "../3d-models/openscad files/container_part.scad" -o "../3d-models/stl files/container_part.stl" -D "export=true;"
+openscad "../3d-models/openscad files/drone_part.scad" -o "../3d-models/stl files/drone_part.stl" -D "export=true;"
+openscad "../3d-models/openscad files/servo_mount.scad" -o "../3d-models/stl files/servo_mount.stl" -D "export=true;"
+openscad "../3d-models/openscad files/locking_pin.scad" -o "../3d-models/stl files/locking_pin.stl" -D "export=true;"
+
+openscad "../3d-models/openscad files/assembled.scad" -o "../3d-models/stl files/assembled.stl" -D "export=true;"
