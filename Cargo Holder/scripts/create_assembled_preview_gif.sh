a429b2f4a1fd0e4648f1927ca70161f792d98690
Andreas Menzel <mail@andreas-menzel.com>
2022-12-18 16:09:13 +0100

    Create script to create assembled preview gif

---
+++ Cargo Holder/scripts/create_assembled_preview_gif.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script creates an animated gif of the assembled cargo holder mechanism.
+
+mkdir -p .tmp_assembled_preview_gif
+
+for vertical_gap in {000..150..1}
+do
+    openscad "../3d-models/openscad files/assembled.scad" -o ".tmp_assembled_preview_gif/assembled_$vertical_gap.png" -D "vertical_gap=$vertical_gap;" --imgsize 800,600 --camera 80,71,83,52,0,57,718 --render
+done
+
+mkdir ../images/
+convert -delay 2 -loop 0 .tmp_assembled_preview_gif/*.png ../images/assembled_preview.gif
+
+rm -R .tmp_assembled_preview_gif
